0bef60a26d01fd0b9416ad063c47ceaa34cecefa
Krzysztof Radzięta <kustosz142@gmail.com>
2022-11-16 17:11:57 +0100

    nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Nav.css";
 
 function Nav() {
+	const [show, setShow] = useState(false);
+
+	const transitionNavBar = () => {
+		//when u scroll more than 1 vh
+		if (window.scrollY > 100) {
+			setShow(true);
+		} else {
+			setShow(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", transitionNavBar);
+		return () => window.removeEventListener("scroll", transitionNavBar); //cleanup function is special important when u touch a listener function
+	}, []);
+
 	return (
-		<div className='nav'>
+		<div className={`nav ${show && "nav__black"}`}>
 			<div className='nav__contents'>
 				<img
 					className='nav__log'
-					src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png'
+					src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
 					alt='Netflix logo.'
 				/>
 				<img
@@ -17,8 +33,6 @@ function Nav() {
 					alt='Your avatar'
 				/>
 			</div>
-
-			<h1>NAV</h1>
 		</div>
 	);
 }
