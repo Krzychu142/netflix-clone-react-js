bce9db1532c7e75228ae2c61f6d524ddc2cbbfc9
Krzysztof Radzięta <kustosz142@gmail.com>
2022-11-23 22:11:10 +0100

    after deploy and add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
 
 import "./App.css";
@@ -7,7 +8,13 @@ import "./App.css";
 function App() {
 	return (
 		<div className='app'>
-			<HomeScreen />
+			<Router>
+				<Routes>
+					<Route exact path='/' element={<HomeScreen />}>
+						{/* this is a root for the home screen */}
+					</Route>
+				</Routes>
+			</Router>
 		</div>
 	);
 }
