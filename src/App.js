aec4d44c8f0152999730ef3b10a716672e7a401a
Krzysztof Radzięta <kustosz142@gmail.com>
2022-11-23 23:13:48 +0100

    start of making login screen

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 import "./App.css";
 
 function App() {
+	const user = null;
 	return (
 		<div className='app'>
 			<Router>
-				<Routes>
-					<Route exact path='/' element={<HomeScreen />}>
-						{/* this is a root for the home screen */}
-					</Route>
-				</Routes>
+				{!user ? (
+					<LoginScreen />
+				) : (
+					<Routes>
+						<Route exact path='/' element={<HomeScreen />}>
+							{/* this is a root for the home screen */}
+						</Route>
+					</Routes>
+				)}
 			</Router>
 		</div>
 	);
