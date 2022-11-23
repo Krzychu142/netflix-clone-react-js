3af5e10462dc6aedf08b6a462655f2fe118adb18
Krzysztof Radzięta <kustosz142@gmail.com>
2022-11-23 22:37:19 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./Screens/HomeScreen";
 
 import "./App.css";
 
