3c5dc63623c4d19c3a23cdff6078a006f67a3844
Krzysztof Radzięta <kustosz142@gmail.com>
2022-11-25 10:36:32 +0100

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen";
 import LoginScreen from "./Screens/LoginScreen";
 
 import "./App.css";
+import { auth } from "./firebase";
 
 function App() {
 	const user = null;
+
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+			if (userAuth) {
+				// Logged in
+				console.log(userAuth);
+			} else {
+				// Logged out
+			}
+		}); //listener of authentication change
+		return unsubscribe;
+	}, []);
+
 	return (
 		<div className='app'>
 			<Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,9 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
+	//that is like some key to authentication
 	apiKey: "AIzaSyDp1rMq3CPn8UT2aaM01E5UWp__PpUUrKw",
 	authDomain: "netflix-clone-73ff4.firebaseapp.com",
 	projectId: "netflix-clone-73ff4",
@@ -6,3 +11,9 @@ const firebaseConfig = {
 	messagingSenderId: "216757868267",
 	appId: "1:216757868267:web:c99846d8895543b4fc90ae",
 };
+
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+const db = firebaseapp.firestore(); // real time database to keep track on state of user subscription
+const auth = firebase.auth();
+
+export { auth, db };
